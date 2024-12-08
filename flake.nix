4ec0761d2ec8240819956e4a73caea3a5d141917
wxt <3264117476@qq.com>
2024-12-08 20:29:59 +0800

    Init

--- flake.nix
+++ flake.nix
@@ -2,11 +2,10 @@
   description = "A Nix-flake-based OI environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    luogu-gcc.url = "github:luogu-dev/judge-env";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self , nixpkgs, luogu-gcc ,... }: let
+  outputs = { self , nixpkgs, ... }: let
     system = "x86_64-linux";
   in {
     devShells."${system}".default = let
